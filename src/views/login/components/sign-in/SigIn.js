0685087f9f2e19940f900d2286c8c5014847486c
IgnasBarakauskas <b.ignas05@gmail.com>
2022-01-30 00:11:36 +0100

    working sign in and sign up views

--- src/views/login/components/sign-in/SigIn.js
+++ src/views/login/components/sign-in/SigIn.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react"
 import { CustomTextField, CustomButton, icon } from "../../../../common/components"
+import regex from "../../../../common/utils"
 import styles from "../SignInOut.module.css"
 
 const SignIn = ({ open }) => {
     const [showPassword, setShowPassWord] = useState(false)
     const [password, setPassword] = useState("")
+    const [passwordErr, setPasswordErr] = useState("")
     const [email, setEmail] = useState("")
+    const [emailErr, setEmailErr] = useState("")
     const handleChangeShowPassword = () => {
         setShowPassWord(!showPassword)
     }
     const handlePassword = (value) => {
         setPassword(value)
+        if (value.length < 4) {
+            setPasswordErr("*Your password is too short")
+        } else {
+            setPasswordErr("")
+        }
     }
     const handleEmail = (value) => {
         setEmail(value)
+        if (!regex.emailRegex.test(value)) {
+            setEmailErr("*Your email is not valid")
+        } else {
+            setEmailErr("")
+        }
     }
     return (
-        <div className={`${styles.container} ${styles["container--left"]}`} data-fullWidth={open}>
+        <div className={`${styles.container} ${styles["container--left"]}`} data-fullwidth={open}>
             <div className={styles.primaryText}>Login to your account</div>
             <div className={styles.secondaryText}>And continue from where you left off </div>
             <div className={styles.subContainer}>
@@ -26,7 +39,7 @@ const SignIn = ({ open }) => {
                     placeholder="Email"
                     className={styles.textField}
                 />
-                <div className={styles.errorText}>*error</div>
+                <div className={styles.errorText}>{emailErr || " "}</div>
                 <CustomTextField
                     placeholder="Password"
                     value={password}
@@ -36,7 +49,7 @@ const SignIn = ({ open }) => {
                     className={styles.textField}
                     icon={showPassword ? icon.faEye : icon.faEyeSlash}
                 />
-                <div className={styles.errorText}>*error</div>
+                <div className={styles.errorText}>{passwordErr || " "}</div>
             </div>
             <div className={styles.subContainer}>
                 <CustomButton className={styles.button} size="xl" color="secondary">
--- src/views/login/components/sign-up/SignUp.js
+++ src/views/login/components/sign-up/SignUp.js
@@ -1,30 +1,58 @@
 import React, { useState } from "react"
 import { CustomButton, CustomTextField, icon } from "../../../../common/components"
+import regex from "../../../../common/utils"
 import styles from "../SignInOut.module.css"
 
 const SignUp = ({ open }) => {
     const [showPassword, setShowPassWord] = useState(false)
     const [email, setEmail] = useState("")
+    const [emailErr, setEmailErr] = useState("")
     const [name, setName] = useState("")
+    const [nameErr, setNameErr] = useState("")
     const [password, setPassword] = useState("")
+    const [passwordErr, setPasswordErr] = useState("")
     const [rePassword, setRePassword] = useState("")
+    const [rePasswordErr, setRePasswordErr] = useState("")
     const handleChangeShowPassword = () => {
         setShowPassWord(!showPassword)
     }
     const handleEmail = (value) => {
         setEmail(value)
+        if (!regex.emailRegex.test(value)) {
+            setEmailErr("*Your email is not valid")
+        } else {
+            setEmailErr("")
+        }
     }
     const handleName = (value) => {
         setName(value)
+        if (value.length < 4) {
+            setNameErr("*Your full name is too short")
+        } else {
+            setNameErr("")
+        }
     }
     const handlePassword = (value) => {
         setPassword(value)
+        if (rePassword === value) {
+            setRePasswordErr("")
+        }
+        if (value.length < 4) {
+            setPasswordErr("*Your password is too short")
+        } else {
+            setPasswordErr("")
+        }
     }
     const handleRePassword = (value) => {
         setRePassword(value)
+        if (password !== value) {
+            setRePasswordErr("*Your passwords are not matching")
+        } else {
+            setRePasswordErr("")
+        }
     }
     return (
-        <div className={`${styles.container} ${styles["container--right"]}`} data-fullWidth={open}>
+        <div className={`${styles.container} ${styles["container--right"]}`} data-fullwidth={open}>
             <div className={styles.primaryText}>Create a new account</div>
             <div className={styles.secondaryText}>And start your journey with us</div>
             <div className={styles.subContainer}>
@@ -34,14 +62,14 @@ const SignUp = ({ open }) => {
                     placeholder="Email"
                     className={styles.textField}
                 />
-                <div className={styles.errorText}>*error</div>
+                <div className={styles.errorText}>{emailErr || " "}</div>
                 <CustomTextField
                     onChange={handleName}
                     value={name}
                     placeholder="Full name"
                     className={styles.textField}
                 />
-                <div className={styles.errorText}>*error</div>
+                <div className={styles.errorText}>{nameErr || " "}</div>
                 <CustomTextField
                     value={password}
                     onChange={handlePassword}
@@ -51,7 +79,7 @@ const SignUp = ({ open }) => {
                     className={styles.textField}
                     icon={showPassword ? icon.faEye : icon.faEyeSlash}
                 />
-                <div className={styles.errorText}>*error</div>
+                <div className={styles.errorText}>{passwordErr || " "}</div>
                 <CustomTextField
                     value={rePassword}
                     onChange={handleRePassword}
@@ -61,7 +89,7 @@ const SignUp = ({ open }) => {
                     className={styles.textField}
                     icon={showPassword ? icon.faEye : icon.faEyeSlash}
                 />
-                <div className={styles.errorText}>*error</div>
+                <div className={styles.errorText}>{rePasswordErr || " "}</div>
             </div>
             <div className={styles.subContainer}>
                 <CustomButton className={styles.button} size="xl" color="secondary">
