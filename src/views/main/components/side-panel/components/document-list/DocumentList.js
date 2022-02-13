c51245a11207bd9c5edb4e7940a3207e19f90e57
Sara ALEMAYEHU <assefa.sara12@gmail.com>
2022-02-13 12:27:27 +0100

    initialized the delet buton

--- src/views/main/components/side-panel/components/document-list/DocumentList.js
+++ src/views/main/components/side-panel/components/document-list/DocumentList.js
@@ -1,13 +1,21 @@
 import React from "react"
+import { CustomIconButton, icon } from "../../../../../../common/components"
+import styles from "./DocumentList.module.css"
+// import { getUserId } from "../../../../../../common/utils"
 
 export default function DocumentList({ documents }) {
+    //  const [delet, setDelet] = useState(true)
+    //  const handleDelet = () => {
+    //      if (user_id === getUserId) setDelet(delet)
+    //  }
     return (
         <span>
             {Array.isArray(documents) &&
                 documents.length > 0 &&
-                documents.map((document, index) => (
-                    <div key={document._id}>
-                        {document.name} {index}
+                documents.map((document) => (
+                    <div key={document._id} className={styles.mainDocument} style={{ cursor: "pointer" }}>
+                        <CustomIconButton icon={icon.faMinus} />
+                        {document.name}
                     </div>
                 ))}
         </span>
